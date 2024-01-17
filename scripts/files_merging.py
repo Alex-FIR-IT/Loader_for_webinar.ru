1011f60abbef019c1887d5e6e84e942e088c6a89
zhandrov@firsov-vostro-3490 <osintfir@gmail.com>
2024-01-17 16:58:52 +0300

    create decorator 'retry_execution_if_exception_is_raised'

--- scripts/files_merging.py
+++ scripts/files_merging.py
@@ -2,7 +2,7 @@ import os
 import re
 from typing import List, AnyStr
 from moviepy.editor import VideoFileClip, concatenate_videoclips
-from support.decorators import print_execution_time, chime_when_is_done
+from support.decorators import print_execution_time, chime_when_is_done, retry_execution_if_exception_is_raised
 
 
 def merging_files_is_needed_from_user() -> bool:
--- support/decorators.py
+++ support/decorators.py
@@ -1,6 +1,6 @@
 import time
 from functools import wraps
-from typing import Callable, AnyStr
+from typing import Callable, AnyStr, Union
 import chime
 
 
@@ -33,6 +33,7 @@ def chime_when_is_done(*, chime_level: AnyStr):
     :param chime_level: 'success' or 'info' or 'warning' or 'error'
     :return: wrapper which additionally chime after function is accomplished
     """
+
     def wrapper_1(func: Callable):
         @wraps(wrapped=func)
         def wrapper_2(*args, **kwargs):
@@ -47,6 +48,35 @@ def chime_when_is_done(*, chime_level: AnyStr):
                 chime.warning()
             else:
                 chime.error()
+
+            return result
+
+        return wrapper_2
+
+    return wrapper_1
+
+
+def retry_execution_if_exception_is_raised(*, retries: int, delay: Union[int, AnyStr]):
+    def wrapper_1(func: Callable):
+        @wraps(wrapped=func)
+        def wrapper_2(*args, **kwargs):
+            successfully = error = result = False
+
+            for retry in range(retries):
+                try:
+                    result = func(*args, **kwargs)
+                    successfully = True
+                    break
+                except Exception as error:
+                    print(f"Попытка {retry + 1} провалилась! \n"
+                          f"Возникло исключение - {error}\n"
+                          )
+                    chime.warning()
+                    time.sleep(secs=delay)
+
+            if not successfully:
+                print('Не удалось выполнить операцию!')
+                raise Exception(error)
 
             return result
 
