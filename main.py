1c79bb8a3396daa22d3b2bf9ba9ea8ca325de3ac
zhandrov@firsov-vostro-3490 <osintfir@gmail.com>
2024-01-17 15:02:45 +0300

    add opportunity to start downloading and merging separately using main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import json
+import re
 from typing import Dict
 from scripts.download import download_webinar
-from scripts.files_merging import merge_files, get_merge_files_or_not_from_user
+from scripts.files_merging import merge_files, get_merge_files_or_not_from_user, main_merge_files
 
 
 def load_from_json() -> Dict:
@@ -29,17 +30,20 @@ def use_only_merging_from_user():
 
 
 def main():
-    filenames_dict = download_webinar()
-
-    script_settings = load_from_json()
-
-    if script_settings.get('auto_files_merging'):
-        merge_files(video_filenames=filenames_dict.get('chunks_filenames'),
-                    filename=filenames_dict.get('webinar_filename'))
+    if use_only_merging_from_user():
+        main_merge_files()
     else:
-        if get_merge_files_or_not_from_user():
+        filenames_dict = download_webinar()
+
+        script_settings = load_from_json()
+
+        if script_settings.get('auto_files_merging'):
             merge_files(video_filenames=filenames_dict.get('chunks_filenames'),
                         filename=filenames_dict.get('webinar_filename'))
+        else:
+            if get_merge_files_or_not_from_user():
+                merge_files(video_filenames=filenames_dict.get('chunks_filenames'),
+                            filename=filenames_dict.get('webinar_filename'))
 
 
 if __name__ == '__main__':
