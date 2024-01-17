9190ed4d543b0e6f121f661ba8b9147228d186cf
zhandrov@firsov-vostro-3490 <osintfir@gmail.com>
2024-01-17 15:29:51 +0300

    add decorator 'chime_when_is_done'

--- support/decorators.py
+++ support/decorators.py
@@ -1,6 +1,7 @@
 import time
 from functools import wraps
 from typing import Callable, AnyStr
+import chime
 
 
 def print_execution_time(*, action: AnyStr):
@@ -18,6 +19,29 @@ def print_execution_time(*, action: AnyStr):
             end = time.time()
 
             print(f'На {action} затрачено {end - start} секунд')
+
+            return result
+
+        return wrapper_2
+
+    return wrapper_1
+
+
+def chime_when_is_done(*, chime_level: AnyStr):
+    def wrapper_1(func):
+        @wraps(wrapped=func)
+        def wrapper_2(*args, **kwargs):
+
+            result = func(*args, **kwargs)
+
+            if chime_level == 'success':
+                chime.success()
+            elif chime_level == 'info':
+                chime.info()
+            elif chime_level == 'warning':
+                chime.warning()
+            else:
+                chime.error()
 
             return result
 
